3913599d6f778eb5319307dccfe53a3c6bc6738b
srikanth141199 <60726437+srikanth141199@users.noreply.github.com>
2023-04-15 19:10:20 +0530

    2 files modified

--- ownModule/own_Module.js
+++ ownModule/own_Module.js
@@ -1,9 +1,11 @@
 var maxTime = 1000;
-var name = "Srikanth";
+var name = 'Srikanth';
 
-var sub = (a,b) =>{
-  return a-b;
-}
+var sub = (a, b) => {
+  return a - b;
+};
+
+console.log(sub);
 
 // Left hand name is external name, imported files need to use left side names to access those fiels from the exported module
 // fields need to be exported to able to acces from child modules/other .js files
